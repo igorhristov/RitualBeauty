88b96104b9718eb8da2d2b81b0b5919ab0f6dfe6
igorhristov <igorhristov@sourcemxacademy.mk>
2020-07-29 19:29:48 +0200

    Updates

---
+++ precache-manifest.2a1d70a7c056d7daa014f3c1be16d792.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f343eabd76dec647644e377d20cb94e0",
+    "url": "/RitualBeauty/index.html"
+  },
+  {
+    "revision": "fda849bb5bd10407a4cb",
+    "url": "/RitualBeauty/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22dc19c44088b743fb6d",
+    "url": "/RitualBeauty/static/css/main.57292a17.chunk.css"
+  },
+  {
+    "revision": "fda849bb5bd10407a4cb",
+    "url": "/RitualBeauty/static/js/2.a8067b01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RitualBeauty/static/js/2.a8067b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dc19c44088b743fb6d",
+    "url": "/RitualBeauty/static/js/main.f15d2843.chunk.js"
+  },
+  {
+    "revision": "9289d23c4e7ae6fe801b",
+    "url": "/RitualBeauty/static/js/runtime-main.69c5a060.js"
+  },
+  {
+    "revision": "a63d332e052217eab0e8a85b9b185ac0",
+    "url": "/RitualBeauty/static/media/01.a63d332e.jpg"
+  },
+  {
+    "revision": "2d6aa2d90b5c4972ae6c41be04def91b",
+    "url": "/RitualBeauty/static/media/02.2d6aa2d9.jpg"
+  },
+  {
+    "revision": "fd4397534e271bdffc1647422b60b96f",
+    "url": "/RitualBeauty/static/media/03.fd439753.jpg"
+  },
+  {
+    "revision": "b4eaff025d81bccdfa0ba3538c59cdd0",
+    "url": "/RitualBeauty/static/media/04.b4eaff02.jpg"
+  },
+  {
+    "revision": "6217929763c28b211054ce1a257a6ee0",
+    "url": "/RitualBeauty/static/media/05.62179297.jpg"
+  },
+  {
+    "revision": "f26dee5f0a08d14c52506c3227e5e42b",
+    "url": "/RitualBeauty/static/media/06.f26dee5f.jpg"
+  },
+  {
+    "revision": "c4d5ee5d472aa6829c976ae56f8a0953",
+    "url": "/RitualBeauty/static/media/1.c4d5ee5d.jpg"
+  },
+  {
+    "revision": "c84164735e2beab2f45218a4a7b46ddb",
+    "url": "/RitualBeauty/static/media/2.c8416473.jpg"
+  },
+  {
+    "revision": "97c640c6d3281b9dbb7461f233dc6d13",
+    "url": "/RitualBeauty/static/media/3.97c640c6.jpg"
+  },
+  {
+    "revision": "eb0949e093205ce1f06a073be4a2743f",
+    "url": "/RitualBeauty/static/media/519 (1).eb0949e0.jpg"
+  },
+  {
+    "revision": "eb0949e093205ce1f06a073be4a2743f",
+    "url": "/RitualBeauty/static/media/519.eb0949e0.jpg"
+  },
+  {
+    "revision": "5465058e1b01d111547c10a1ca5b7575",
+    "url": "/RitualBeauty/static/media/520.5465058e.jpg"
+  },
+  {
+    "revision": "55b18d7dc091faf14dd8c1ca63c41e3f",
+    "url": "/RitualBeauty/static/media/532.55b18d7d.jpg"
+  },
+  {
+    "revision": "0a0c38d0dcf743193f5fc9fa64b75716",
+    "url": "/RitualBeauty/static/media/533.0a0c38d0.jpg"
+  },
+  {
+    "revision": "d4002f0ea07d9d540a480bfb789f1948",
+    "url": "/RitualBeauty/static/media/glamuroznaSminka.d4002f0e.jpg"
+  },
+  {
+    "revision": "8cf07ac2dadccf2a5668de9112ece3e3",
+    "url": "/RitualBeauty/static/media/header-img.8cf07ac2.jpg"
+  },
+  {
+    "revision": "4d80507e39a1594a060f816ce17531a9",
+    "url": "/RitualBeauty/static/media/img1920x900.4d80507e.jpg"
+  },
+  {
+    "revision": "e41ecfb808c7e935869816c1752b2dd7",
+    "url": "/RitualBeauty/static/media/nevestinska.e41ecfb8.jpg"
+  },
+  {
+    "revision": "93c83fb8e3b267bed5fe196aa0b29e30",
+    "url": "/RitualBeauty/static/media/nevestinskaSminka.93c83fb8.jpg"
+  },
+  {
+    "revision": "0764111d4c6a782cac2cb823605c4fe7",
+    "url": "/RitualBeauty/static/media/salon.0764111d.jpg"
+  },
+  {
+    "revision": "06bc149d0d33b2d8f7eea489abf1831f",
+    "url": "/RitualBeauty/static/media/sminka.06bc149d.jpg"
+  },
+  {
+    "revision": "e3a2faad1d0d9690b445df042fd83c1d",
+    "url": "/RitualBeauty/static/media/staf1.e3a2faad.jpg"
+  },
+  {
+    "revision": "f1aee8d16d52ae8344ef23f83192f157",
+    "url": "/RitualBeauty/static/media/staf3.f1aee8d1.jpg"
+  },
+  {
+    "revision": "e5a16664ae19a8a35f724e4d31f809b3",
+    "url": "/RitualBeauty/static/media/staf4.e5a16664.jpg"
+  }
+]);
